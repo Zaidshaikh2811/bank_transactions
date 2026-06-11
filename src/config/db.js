b8d8aa25ac704b822334a67837cfc528b36c8453
zaidshaikh2811 <zaidshaikh2811@gmail.com>
2026-06-11 20:49:10 +0530

    adding logs

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 
 
 function connectDB() {
+    console.log("MONGO_URI:", process.env.MONGO_URI);
     mongoose.connect(process.env.MONGO_URI).then(() => {
         console.log("Connected to MongoDB");
     }).catch((err) => {
